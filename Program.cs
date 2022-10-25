1ef2896c075c6d8bb8364461e0c0dcca214cb131
cp <cp@DESKTOP-JNI44N7>
2022-10-25 17:32:17 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//Zad1
+/*Console.WriteLine("Wszystkie dni listopada: ");
+for (int i = 1; i < 31; i++)
+    Console.WriteLine(i);
+*/
+//Zad2
+/*int a = int.Parse(System.Console.ReadLine());
+for (int i = 1; i <= a; i++)
+    if (i % 2 != 0) System.Console.WriteLine(i * i);*/
+//Zad3
+/*for (int i = 1000; i <= 10000; i++)
+    if (i % 379 == 0) System.Console.WriteLine(i);*/
+//Zad4
+/*for (int i = 100; i <= 1000; i++)
+    if (i % 5 == 0) Console.WriteLine(i);
+for (int i = 100; i <= 1000; i++)
+    if (i % 6 == 0) Console.WriteLine(i);
+for (int i = 100; i <= 1000; i++)
+    if (i % 11 == 0) Console.WriteLine(i);*/
+//Zad5
+/*Console.WriteLine("Podaj ile chcesz dodać liczb: ");
+int n = int.Parse(System.Console.ReadLine());
+int suma = 0;
+for(int i = 0; i < n; i++)
+    {
+    int a = int.Parse(System.Console.ReadLine());
+    suma = suma += a;
+}
+System.Console.WriteLine($"Suma wynosi: {suma}");*/
+//Zad6
+/*Console.WriteLine("Podaj k: ");
+int k = int.Parse(Console.ReadLine());
+int suma = 0;
+for (int i = 0; i < k; i++)
+    suma = k * 2;
+Console.WriteLine($"Suma wynosi: {suma}");*/
+//Zad 8
+/*Console.WriteLine("Podaj W0: ");
+int W0 = int.Parse(System.Console.ReadLine());
+Console.Write("Podaj L : ");
+int L = int.Parse(System.Console.ReadLine());
+int suma = W0;
+for (int i = 0; i < (L * 12); i++)
+{
+    int w = suma * 0.06 * 1 / 12f;
+    suma = suma + y;
+}
+Console.WriteLine($"Wartość inwestycji wynosi: {Math.Round(suma, 2)}");*/
+
+
+
+
+
